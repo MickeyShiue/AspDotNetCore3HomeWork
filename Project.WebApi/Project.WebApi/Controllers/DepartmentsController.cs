eca5da57d414530bac54f134f182df3103465229
MickeyShiue <e031304@gmail.com>
2020-05-20 23:13:46 +0800

    1、增加 Dep 與 Person api controller 功能

---
+++ Project.WebApi/Project.WebApi/Controllers/DepartmentsController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Project.DataAccess.Lib.Models;
+using Project.DataAccess.Lib.UnitOfWork;
+using Project.Service.Lib.DepartmentService;
+
+namespace Project.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly IDepartmentService departmentService;
+        private readonly IUnitOfWork unitOfWork;
+
+        public DepartmentsController(ContosouniversityContext context,
+            IDepartmentService departmentService,
+            IUnitOfWork unitOfWork)
+        {     
+            this.departmentService = departmentService;
+            this.unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Departments
+        [HttpGet]
+        public ActionResult<IEnumerable<Department>> GetDepartment()
+        {
+            return this.departmentService.GetAll(r => r.IsDeleted == null || r.IsDeleted == false).ToList();
+        }
+
+        // GET: api/Departments/5
+        [HttpGet("{id}")]
+        public ActionResult<Department> GetDepartment(int id)
+        {
+            var Department = this.departmentService.GetAll(
+                r => (r.IsDeleted == false || r.IsDeleted == null) && r.DepartmentId == id).FirstOrDefault();
+
+            if (Department == null)
+            {
+                return NotFound("此筆資料已刪除或不存在");
+            }
+
+            return Department;
+        }
+       
+        [HttpPut("{id}")]
+        public IActionResult PutDepartment(int id, Department department)
+        {
+            department.DateModified = DateTime.Now;
+            this.departmentService.Update(department);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public ActionResult<Department> PostDepartment(Department department)
+        {
+            this.departmentService.Create(department);
+            return Ok(department);
+        }
+
+        // DELETE: api/Departments/5
+        [HttpDelete("{id}")]
+        public ActionResult<Department> DeleteDepartment(int id)
+        {
+            var Department = this.departmentService.GetAll(r => r.DepartmentId == id).FirstOrDefault();
+            if (Department == null)
+            {
+                return NotFound();
+            }
+            Department.IsDeleted = true;
+
+            this.departmentService.Update(Department);
+            return Department;
+        }
+
+        // GET: api/GetDepartmentCourseCount
+        [HttpGet("GetDepartmentCourseCount")]
+        public ActionResult<IEnumerable<VwDepartmentCourseCount>> GetDepartmentCourseCount()
+        {
+            return this.departmentService.GetVwDepartmentCourseCount().ToList();
+        }
+    }
+}
---
+++ Project.WebApi/Project.WebApi/Controllers/PersonController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Project.DataAccess.Lib.Models;
+using Project.DataAccess.Lib.UnitOfWork;
+using Project.Service.Lib.PersonServices;
+
+namespace Project.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+     
+        private readonly IPersonService personService;
+        private readonly IUnitOfWork unitOfWork;
+
+        public PersonController(IPersonService personService, IUnitOfWork unitOfWork)
+        {
+            this.personService = personService;
+            this.unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Person
+        [HttpGet]
+        public ActionResult<IEnumerable<Person>> GetPerson()
+        {
+            return personService.GetAll(r => r.IsDeleted == false || r.IsDeleted == null).ToList();
+        }
+
+        // GET: api/Person/5
+        [HttpGet("{id}")]
+        public ActionResult<Person> GetPerson(int id)
+        {
+            var person = this.personService.GetAll(
+               r => (r.IsDeleted == false || r.IsDeleted == null) && r.Id == id).FirstOrDefault();
+
+            if (person == null)
+            {
+                return NotFound("此筆資料已刪除或不存在");
+            }
+
+            return person;
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult PutPerson(int id, Person person)
+        {
+            person.DateModified = DateTime.Now;
+            this.personService.Update(person);
+
+            try
+            {
+                unitOfWork.SaveChange();
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new
+                {
+                    errorno = 1,
+                    message = "我懶得處理，略..."
+                });
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public ActionResult<Person> PostPerson(Person person)
+        {
+            this.personService.Create(person);
+
+            try
+            {
+                unitOfWork.SaveChange();
+            }
+            catch (Exception)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new
+                {
+                    message = "我懶得處理，略..."
+                });
+            }
+
+            return Ok(person);
+        }
+
+        // DELETE: api/Person/5
+        [HttpDelete("{id}")]
+        public ActionResult<Person> DeletePerson(int id)
+        {
+            var person = this.personService.GetAll(r => r.Id == id).FirstOrDefault();
+            if (person == null)
+            {
+                return NotFound();
+            }
+            person.IsDeleted = true;
+
+            this.personService.Update(person);
+            unitOfWork.SaveChange();
+            return person;
+        }
+    }
+}
