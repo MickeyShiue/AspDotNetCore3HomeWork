c6e967f759c3b294ff8c0087ed5477f34426bcf6
MickeyShiue <e031304@gmail.com>
2020-05-20 19:50:09 +0800

    1、產生 POCO Entity

---
+++ Project.WebApi/Project.DataAccess.Lib/Models/ContosouniversityFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Project.DataAccess.Lib.Models
+{
+    public class ContosouniversityFactory : IDesignTimeDbContextFactory<ContosouniversityContext>
+    {
+        public ContosouniversityContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ContosouniversityContext>();
+            optionsBuilder.UseSqlServer("Data Source=ContosoUniversity;Initial Catalog=ContosoUniversity;Integrated Security=True");
+
+            return new ContosouniversityContext(optionsBuilder.Options);
+        }
+    }
+}
